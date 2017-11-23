09f78cb81910675bfd936076b24eea0f3d376d27
you7588 <yo7588@gmail.com>
2017-11-23 19:23:14 +0800

    finish sample6

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
-var posts = require('./routes/posts');
+var page = require('./route.page');
+var api = require('./route.api');
 
 var app = express();
 
@@ -23,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/posts', posts);
+app.use('/', page);
+app.use('/api', api);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
