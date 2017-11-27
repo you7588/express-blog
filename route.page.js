9109df09be4bcf9d9092eb1fabeacb2db97de14c
you7588 <yo7588@gmail.com>
2017-11-27 16:46:32 +0800

    finish sample-9 show.page

--- route.page.js
+++ route.page.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var PostModel = require('./models/post');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +15,15 @@ router.get('/posts', function(req, res, next) {
 /* GET posts edit page. */
 router.get('/posts/create', function(req, res, next) {
   res.render('create');
+});
+
+/* GET show page. */
+router.get('/posts/show', function (req, res, next) {
+  var id = req.query.id;
+
+  PostModel.findOne({_id: id}, function (err, post) {
+    res.render('show', {post});
+  });
 });
 
 module.exports = router;
