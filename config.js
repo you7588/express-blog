78f2a5d5da02ca2cef076bd56191e558916e9bdb
you7588 <yo7588@gmail.com>
2017-12-26 14:18:26 +0800

    change mongodb url

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
   cookieName: 'chenlingmin',
-  mongodbUrl: 'mongodb://192.168.99.100:32768/weapp'
+  mongodbUrl: 'mongodb://192.168.99.100:32770/weapp',
+  admin: 'admin',
+  sessionSecret: '123123'
 }
